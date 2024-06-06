11670246e4091701bd2d946359a16838dea36916
tario-you <tario01pd2024@gmail.com>
2024-06-06 20:43:18 +0900

    fixed mock-aws-s3 imports

--- components/utils_safeplate/require_fix.py
+++ components/utils_safeplate/require_fix.py
@@ -8,7 +8,6 @@ data = {
         '/Users/tarioyou/safeplate3/node_modules/cipher-base/index.js': [(2, 'stream')], 
         '/Users/tarioyou/safeplate3/node_modules/@mapbox/node-pre-gyp/lib/util/versioning.js': [(5, 'path')], 
         '/Users/tarioyou/safeplate3/node_modules/rimraf/rimraf.js': [(3, 'fs'), (2, 'path')], 
-        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/lib/mock.js': [(14, 'path'), (17, 'path'), (13, 'crypto')], 
         '/Users/tarioyou/safeplate3/node_modules/nock/lib/intercept.js': [(10, 'http')], 
         '/Users/tarioyou/safeplate3/node_modules/hash-base/index.js': [(3, 'stream')], 
         '/Users/tarioyou/safeplate3/node_modules/nock/lib/back.js': [(14, 'path')], 
@@ -16,36 +15,40 @@ data = {
         '/Users/tarioyou/safeplate3/node_modules/levelup/lib/write-stream.js': [(7, 'stream')], 
         '/Users/tarioyou/safeplate3/node_modules/level-filesystem/paths.js': [(1, 'path')], 
         '/Users/tarioyou/safeplate3/node_modules/glob/glob.js': [(48, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/index.js': [(23, 'fs')], 
-        '/Users/tarioyou/safeplate3/node_modules/nock/lib/intercepted_request_router.js': [(9, 'https')], 
+        '/Users/tarioyou/safeplate3/node_modules/nock/lib/intercepted_request_router.js': [(8, 'http')], 
         '/Users/tarioyou/safeplate3/node_modules/detect-libc/lib/filesystem.js': [(6, 'fs')], 
         '/Users/tarioyou/safeplate3/node_modules/levelup/node_modules/readable-stream/readable.js': [(1, 'stream'), (10, 'stream')], 
         '/Users/tarioyou/safeplate3/node_modules/level-blobs/node_modules/readable-stream/readable.js': [(2, 'stream'), (9, 'stream')], 
         '/Users/tarioyou/safeplate3/node_modules/fwd-stream/node_modules/readable-stream/readable.js': [(1, 'stream'), (10, 'stream')], 
         '/Users/tarioyou/safeplate3/node_modules/glob/common.js': [(13, 'fs'), (14, 'path')], 
         '/Users/tarioyou/safeplate3/node_modules/fs.realpath/index.js': [(8, 'fs')], '/Users/tarioyou/safeplate3/node_modules/glob/sync.js': [(9, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/empty/index.js': [(4, 'fs'), (5, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/move-sync/index.js': [(4, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/output/index.js': [(5, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/move/index.js': [(5, 'path')], 
         '/Users/tarioyou/safeplate3/node_modules/@tradle/react-native-http/lib/request.js': [(1, 'stream')], '/Users/tarioyou/safeplate3/node_modules/https-browserify/index.js': [(1, 'http')], '/Users/tarioyou/safeplate3/node_modules/nock/lib/playback_interceptor.js': [(5, 'zlib'), (3, 'stream')], '/Users/tarioyou/safeplate3/node_modules/levelup/node_modules/readable-stream/lib/_stream_writable.js': [(40, 'stream')], '/Users/tarioyou/safeplate3/node_modules/level-blobs/node_modules/readable-stream/lib/_stream_writable.js': [(40, 'stream')], '/Users/tarioyou/safeplate3/node_modules/levelup/node_modules/readable-stream/lib/_stream_readable.js': [(43, 'stream')], '/Users/tarioyou/safeplate3/node_modules/level-blobs/node_modules/readable-stream/lib/_stream_readable.js': [(43, 'stream')], '/Users/tarioyou/safeplate3/node_modules/fs.realpath/old.js': [(22, 'path'), (24, 'fs')], '/Users/tarioyou/safeplate3/node_modules/fwd-stream/node_modules/readable-stream/lib/_stream_writable.js': [(40, 'stream')], '/Users/tarioyou/safeplate3/node_modules/fwd-stream/node_modules/readable-stream/lib/_stream_readable.js': [(43, 'stream')], '/Users/tarioyou/safeplate3/node_modules/graceful-fs/graceful-fs.js': [(1, 'fs')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/copy/copy.js': [(4, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/ensure/file.js': [(4, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/ensure/link.js': [(4, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/copy-sync/copy-sync.js': [(4, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/ensure/symlink.js': [(4, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/json/output-json.js': [(3, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/json/output-json-sync.js': [(4, 'path')], 
         '/Users/tarioyou/safeplate3/node_modules/browserify-zlib/lib/index.js': [(4, 'stream')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/mkdirs/mkdirs-sync.js': [(4, 'path')], 
         '/Users/tarioyou/safeplate3/node_modules/@tradle/react-native-http/lib/response.js': [(1, 'stream')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/mkdirs/mkdirs.js': [(4, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/util/utimes.js': [(4, 'os')], 
         '/Users/tarioyou/safeplate3/node_modules/graceful-fs/polyfills.js': [(1, 'constants')], 
         '/Users/tarioyou/safeplate3/node_modules/graceful-fs/legacy-streams.js': [(1, 'stream')], 
         '/Users/tarioyou/safeplate3/node_modules/jsonfile/index.js': [(5, 'fs')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/mkdirs/win32.js': [(3, 'path')], 
-        # '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/ensure/symlink-paths.js': [(3, 'path')]
+
+        # mock-aws-s3
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/remove/rimraf.js': [(4, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/lib/mock.js': [(14, 'path'), (17, 'path'), (13, 'crypto')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/index.js': [(23, 'fs')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/empty/index.js': [(4, 'fs'), (5, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/move-sync/index.js': [(4, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/output/index.js': [(5, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/move/index.js': [(5, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/copy/copy.js': [(4, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/ensure/file.js': [(4, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/ensure/link.js': [(4, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/copy-sync/copy-sync.js': [(4, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/ensure/symlink.js': [(4, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/json/output-json.js': [(3, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/json/output-json-sync.js': [(4, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/mkdirs/mkdirs-sync.js': [(4, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/mkdirs/mkdirs.js': [(4, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/util/utimes.js': [(4, 'os'), (5, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/mkdirs/win32.js': [(3, 'path')], 
+        '/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/ensure/symlink-paths.js': [(3, 'path')]
 }
 
 replacements = {
@@ -61,9 +64,12 @@ replacements = {
 }
 
 for k, v in data.items():
+    # if k != "/Users/tarioyou/safeplate3/node_modules/mock-aws-s3/node_modules/fs-extra/lib/util/utimes.js": continue
     with open(k, 'r') as f:
         lines = f.read().split('\n')
+    # print(lines)
     for line_num, keyword in v:
         lines[line_num-1] = lines[line_num-1].replace('\''+keyword+'\'','\''+replacements[keyword]+'\'').replace('\"'+keyword+'\"','\''+replacements[keyword]+'\'')
+        # print(lines[line_num-1])
     with open(k, 'w') as f:
         f.write('\n'.join(lines))
